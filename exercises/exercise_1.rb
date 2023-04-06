693f848e01c24278b2214d4bb043df80bca8a53f
PavelKotlov <pavelkotlov@gmail.com>
2023-04-06 04:13:22 -0700

    Exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,34 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+stores = [
+  {
+    name: "Burnaby", 
+    revenue: 300000, 
+    mens_apparel: true, 
+    womens_apparel: true
+  }, 
+  {
+    name: "Richmond", 
+    revenue: 1260000, 
+    mens_apparel: false, 
+    womens_apparel: true
+  }, 
+  {
+    name: "Gastown", 
+    revenue: 190000, 
+    mens_apparel: true, 
+    womens_apparel: false
+  }
+]
+
+stores.each do |current_store|
+  store = Store.new
+  store.name = current_store[:name]
+  store.annual_revenue = current_store[:revenue]
+  store.mens_apparel = current_store[:mens_apparel]
+  store.womens_apparel = current_store[:womens_apparel]
+  store.save
+end
+
+puts Store.count
