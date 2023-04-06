f8fb5fce9af09389120e80c3c450118994534460
PavelKotlov <pavelkotlov@gmail.com>
2023-04-06 04:14:57 -0700

    Exercise 6 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,46 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+employees = [
+  {
+    first_name: "Bob",
+    last_name: "Cat",
+    hourly_rate: 100
+  },
+  {
+    first_name: "John",
+    last_name: "Smith",
+    hourly_rate: 80
+  },
+  {
+    first_name: "Jane",
+    last_name: "Doe",
+    hourly_rate: 180
+  },
+  {
+    first_name: "Larry",
+    last_name: "Jones",
+    hourly_rate: 150
+  },
+  {
+    first_name: "Shoshana",
+    last_name: "Liberman",
+    hourly_rate: 40
+  },
+]
+
+employees.each do |employee|
+  @store1.employees.create(
+    first_name: employee[:first_name], 
+    last_name: employee[:last_name], 
+    hourly_rate: employee[:hourly_rate]
+  )
+end
+
+employees.reverse.each do |employee|
+  @store2.employees.create(
+    first_name: employee[:first_name], 
+    last_name: employee[:last_name], 
+    hourly_rate: employee[:hourly_rate]
+  )
+end
