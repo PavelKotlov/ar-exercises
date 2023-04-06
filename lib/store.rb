d1c4c03ba60959b0dfb3694579f49a02d4e49009
PavelKotlov <pavelkotlov@gmail.com>
2023-04-06 04:15:52 -0700

    Refactor Store and Employee models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
+  validate :carries_at_least_one_type_of_apperal
+
+  def carries_at_least_one_type_of_apperal
+    unless mens_apparel.present? || womens_apparel.present?
+      errors.add(:apparel, "not assigned, each store must carry at least one type of appreal, mens or womens")
+    end
+  end
 end
